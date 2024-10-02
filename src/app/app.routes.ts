7a3181a0c52601c13ab375b94b0ca6aee10efd99
Yamir Vargas Alvitres <150144552+ALVI3ZZZ@users.noreply.github.com>
2024-10-02 18:58:23 +0000

    Iniciar sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';  // Importa el componente de iniciar sesión
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full' },  // Redirecciona a iniciar sesión por defecto
+  { path: 'iniciar-sesion', component: IniciarSesionComponent },  // Ruta para el componente de iniciar sesión
+  { path: '**', redirectTo: 'iniciar-sesion' }  // Redirección por defecto a "iniciar-sesion" en caso de rutas no encontradas
+];
