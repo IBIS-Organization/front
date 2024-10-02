35a938cf9f0cdbf1f5d0bd327041f7b15071a6ba
FrancoAless <fleonr3@upao.edu.pe>
2024-10-02 20:30:18 +0000

    Primera-versión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';  // Importa el componente de registro
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'registro-usuario', pathMatch: 'full' },  // Redirecciona a "registro-usuario" por defecto
+  { path: 'registro-usuario', component: RegistroUsuarioComponent },  // Ruta para el componente de registro de usuario
+  { path: '**', redirectTo: 'registro-usuario' }  // Redirección por defecto a "registro-usuario" en caso de rutas no encontradas
+];
