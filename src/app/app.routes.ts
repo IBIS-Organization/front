32c51d203cdc3392b9de3cd318e6fc3cbc58d9d5
Yuliana Cacho <ycachoq1@upao.edu.pe>
2024-10-04 02:41:09 -0500

    Termiando el front de la pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './body/body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'nav', component: NavComponent }
+  
+];
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports:[],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-
+  
 }
