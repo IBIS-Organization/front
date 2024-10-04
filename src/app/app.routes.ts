82d21fa9421410e4a14ff0e9d64f765f67ce94b1
FrancoAless <fleonr3@upao.edu.pe>
2024-10-04 16:53:55 +0000

    Seleccion de habitaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component'; // Importa el componente Habitaciones
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'habitaciones', pathMatch: 'full' },  // Redirecciona a iniciar sesión por defecto
+  { path: 'habitaciones', component: HabitacionesComponent },  // Ruta para el componente de iniciar sesión
+  { path: '**', redirectTo: 'habitaciones' }  // Redirección por defecto a "iniciar-sesion" en caso de rutas no encontradas
+];
