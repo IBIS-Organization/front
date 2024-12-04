6ff5b99033f4eb0ca7eb87456dffa2e4dea26d0f
Espinozaje <jeisson12aaron@gmail.com>
2024-12-04 02:28:13 -0500

    final

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://insoback-inso.up.railway.app/'   // URL base para producción
+    apiUrl: 'http://localhost:8080/'   // URL base para producción
   };
